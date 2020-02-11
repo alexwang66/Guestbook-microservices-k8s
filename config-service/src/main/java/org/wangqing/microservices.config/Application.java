cbeadd145f60511950f5cfd16395fb24b654a32d
Q Wang <wangq@jfrog.com>
2020-02-11 15:01:15 +0800

    add import

--- config-service/src/main/java/org/wangqing/microservices.config/Application.java
+++ config-service/src/main/java/org/wangqing/microservices.config/Application.java
@@ -1,7 +1,11 @@
 package org.wangqing.microservices.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
