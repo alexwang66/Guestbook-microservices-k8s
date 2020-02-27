5d016e36194fb49076a6a63b0dbee12ec02fb0cc
Q Wang <wangq@jfrog.com>
2020-02-27 17:18:47 +0800

    fix zipkin

--- runOnLocal.sh
+++ runOnLocal.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
 
-#mvn -f discovery-service/pom.xml package -Dmaven.test.skip=true
-#java -jar discovery-service/target/discovery-service-1.0.jar
-#mvn -f account-service/pom.xml package -Dmaven.test.skip=true
-#java -jar account-service/target/account-service.jar
-#
-#
-#mvn -f gateway-service/pom.xml package -Dmaven.test.skip=true
-#java -jar gateway-service/target/gateway-service.jar
-#
-#mvn -f zipkin-service/pom.xml package -Dmaven.test.skip=true
-#java -jar zipkin-service/target/zipkin-service-1.0.jar
-#
+nohup java -jar discovery-service/target/discovery-service-1.0.jar  &
+sleep 5s # Waits 5 seconds.
+nohup java -jar account-service/target/account-service.jar  &
+nohup java -jar gateway-service/target/gateway-service.jar  &
+nohup java -jar zipkin-service/target/zipkin-service-1.0.jar  &
 
-
- java -jar discovery-service/target/discovery-service-1.0.jar
- java -jar account-service/target/account-service.jar
- java -jar gateway-service/target/gateway-service.jar
- java -jar zipkin-service/target/zipkin-service-1.0.jar
---
+++ stopAll.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+kubectl delete -f kube-deploy/discovery.yaml
+kubectl delete -f kube-deploy/gateway.yaml
+kubectl delete -f kube-deploy/account.yaml
+kubectl delete -f kube-deploy/zipkin.yaml
+
+
+kill -9 $(lsof -n -i4TCP:8761 | grep LISTEN  | awk '{ print $2 }')
+kill -9 $(lsof -n -i4TCP:2222 | grep LISTEN  | awk '{ print $2 }')
+kill -9 $(lsof -n -i4TCP:9411 | grep LISTEN  | awk '{ print $2 }')
+kill -9 $(lsof -n -i4TCP:8765 | grep LISTEN  | awk '{ print $2 }')
