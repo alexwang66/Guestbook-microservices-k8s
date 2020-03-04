3b2113619ee8331b45d3171158c94f5f02997fd8
Q Wang <wangq@jfrog.com>
2020-03-04 15:15:41 +0800

    fix gateway

--- runAll.sh
+++ runAll.sh
@@ -6,7 +6,6 @@ while true; do
     case $yn in
         [Yy]* )
             nohup java -jar discovery-service/target/discovery-service-1.0.jar  &
-            sleep 5s # Waits 5 seconds.
             nohup java -jar account-service/target/account-service.jar  &
             nohup java -jar gateway-service/target/gateway-service.jar  &
             nohup java -jar zipkin-service/target/zipkin-service-1.0.jar  &
